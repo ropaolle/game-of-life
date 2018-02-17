98622c13a69de8d0cd5d7c97982114cea4816e95
ropaolle <ropaolle@gmail.com>
2018-02-17 16:54:17 +0100

    Component cleanup

--- src/app/AppBar.js
+++ src/app/AppBar.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-// import IconButton from 'material-ui/IconButton';
-// import MenuIcon from 'material-ui-icons/Menu';
 
 const styles = theme => ({
   root: {
@@ -17,26 +14,20 @@ const styles = theme => ({
   },
 });
 
-class ButtonAppBar extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <header className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <img src="./favicon-32x32.png" alt="logo" />
-            <Typography variant="title" color="inherit" className={classes.flex}>
+const ButtonAppBar = () => {
+  const { classes } = this.props;
+  return (
+    <header className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <img src="./favicon-32x32.png" alt="logo" />
+          <Typography variant="title" color="inherit" className={classes.flex}>
               Game of Life with React
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </header>
-    );
-  }
-}
-
-ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
 };
 
 export default withStyles(styles)(ButtonAppBar);
